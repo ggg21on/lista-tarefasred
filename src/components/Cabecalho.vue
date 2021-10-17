31cf1bda21d79ba38f213018127037f5728bb64b
Gabriel Gomes Galikosky <gomesgalikosky@gmail.com>
2021-10-17 21:08:11 -0200

    Projeto inicial

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+      <h1>Bem-vindo</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  
+
+}
+</script>
+
+<style>
+ header {
+   background: blue;
+   color:whitesmoke;
+ } 
+</style>
---
+++ src/components/Rodape.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>Rodapé</footer>
+</template>
+
+<script>
+export default {}
+
+</script>
+
+<style scoped>
+footer {
+    background: blue;
+    color: whitesmoke;
+    height: 50px;
+    width: 100%;
+    position: fixed;
+    bottom: 0px;
+}
+
+
+</style>
